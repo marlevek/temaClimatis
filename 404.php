9e667e65851aa0bcf94f8357d6e4905ed056f691
marlevek <marcelo_levek@hotmail.com>
2020-07-21 19:51:03 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<?php wp_head(); ?>
-<?php get_header(); ?>
-<style>
-@import url('https://fonts.googleapis.com/css?family=Lacquer&display=swap');
-</style>
+  <?php wp_head(); ?>
+  <?php get_header(); ?>
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Lacquer&display=swap');
+  </style>
 </head>
+
 <body <?php body_class(); ?>>
   <div class="jumbotron mb-3">
     <div class="col-12">
-      <p class="h3 text-center titulo fonte" >Ops!</p>
-      <p class="h3 text-center titulo fonte" >Tá muito frio, me tire daqui.</p>
-      <img class="img-fluid mx-auto d-block" src="http://localhost/clima620_climatis-blog_wp/wp-content/uploads/2020/03/Pinguim_frio.png" alt="Imagem Pinguim - Erro 404">
-      <h3 class="titulo fonte" >ERRO 404!</h3>
+      <p class="h3 text-center titulo fonte">Ops!</p>
+      <p class="h3 text-center titulo fonte">Tá muito frio, me tire daqui.</p>
+      <img class="img-fluid mx-auto d-block" src="https://www.climatis.com.br/Blog/wp-content/uploads/2020/03/Pinguim_frio.png" alt="Imagem Pinguim - Erro 404">
+      <h3 class="titulo fonte">ERRO 404!</h3>
     </div>
   </div>
 </body>
+
 </html>
 <?php get_footer(); ?>
